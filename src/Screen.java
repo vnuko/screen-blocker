270b503f76e5bdccf5eb18031381b84c18606527
Milos Vnucko <milosz.milosz@gmail.com>
2021-02-23 18:19:12 +0100

    added title and code refactor

--- src/Screen.java
+++ src/Screen.java
@@ -3,6 +3,10 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 import java.util.Timer;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -25,11 +29,30 @@ class Screen implements KeyListener {
 
     static String timeStr;
 
+    static Logger logger;
+    static FileHandler fh;
+
     // main class
     public static void main(String[] args) throws IOException {
         System.setProperty("apple.awt.UIElement", "true");
 
+        logger = Logger.getLogger(Screen.class.getName());
+        fh = null;
+
+        try {
+            fh = new FileHandler("screen-blocker.log");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        fh.setFormatter(new SimpleFormatter());
+        logger.addHandler(fh);
+
         properties = Screen.getProperties();
+
+        if (!properties.getProperty("debug", "false").equals("true")) {
+            LogManager.getLogManager().reset(); // disable logging
+        }
 
         try {
             UIManager.setLookAndFeel(
@@ -51,17 +74,15 @@ class Screen implements KeyListener {
         frame.setResizable(false);
         frame.setBackground(new Color(0,0,0));
 
-        //device.setFullScreenWindow(frame); //DEBUG
-
         // Background Component
         background = new JLabel();
         background.setIcon(
                 new ImageIcon(
                         new ImageIcon(
                                 ImageIO.read(
-                                        new File("resources/images/stop.jpg")
+                                        new File("screen-blocker.jpg")
                                 )
-                        ).getImage().getScaledInstance((int) size.getWidth(), (int) size.getHeight(), Image.SCALE_SMOOTH)
+                        ).getImage().getScaledInstance((int) size.getWidth(), (int) size.getHeight(), Image.SCALE_DEFAULT)
                 )
         );
         background.setLayout(null);
@@ -92,7 +113,7 @@ class Screen implements KeyListener {
         message.setFont(
                 new Font("SansSerif", Font.PLAIN, 100)
         );
-        message.setForeground(new Color(0, 0, 0, 220));
+        message.setForeground(new Color(255, 255, 255));
         message.setBounds(
             (int) ((size.getWidth() / 2) - (message.getPreferredSize().getWidth() / 2)),
             (int) ((size.getHeight() / 2) - message.getPreferredSize().getHeight() - 60),
@@ -101,6 +122,8 @@ class Screen implements KeyListener {
         );
 
         background.add(message);
+
+        logger.info("Starting timer...");
 
         Timer timer = new Timer();
         TimerTask tt = new TimerTask() {
@@ -121,25 +144,38 @@ class Screen implements KeyListener {
                 int hour = cal.get(Calendar.HOUR_OF_DAY);
                 int min = cal.get(Calendar.MINUTE);
 
+                logger.info("Current time: " + hour  + ":" + min);
+
                 if(!timeStr.equals("")) {
+
+                    logger.info("Time configurations for today: " + timeStr);
+
                     StringTokenizer tokens = new StringTokenizer(timeStr, ";");
                     while (tokens.hasMoreElements()) {
                         String hhmmStr = tokens.nextElement().toString();
                         String[] hhmmArr = hhmmStr.split(":");
+                        logger.info("Checking configuration time against current time...");
                         if (hour == Integer.parseInt(hhmmArr[0]) && min == Integer.parseInt(hhmmArr[1])) {
+                            logger.info("FOUND match, current time " + hour  + ":" + min + " matches config's: " + Integer.parseInt(hhmmArr[0]) + ":" + Integer.parseInt(hhmmArr[1]));
+                            logger.info("Showing Screen Blocker window");
                             device.setFullScreenWindow(frame);
+                            break;
+                        } else {
+                            logger.info("NO match, will try again in 60 sec. Current time: " + hour  + ":" + min + " didn't match config's: " + Integer.parseInt(hhmmArr[0]) + ":" + Integer.parseInt(hhmmArr[1]));
                         }
                     }
                 }
             }
         };
-        timer.schedule(tt, 1000, 1000*60);//	delay the task 1 second, and then run task every five seconds
+        timer.schedule(tt, 1000, 1000*60);//	delay the task 1 second, and then run task every 60 seconds
+
+        device.setFullScreenWindow(frame); //DEBUG
     }
     
     public static Properties getProperties() throws IOException {
         Properties p = new Properties();
 
-        InputStream inputstream = new FileInputStream("resources/config/properties.config");
+        InputStream inputstream = new FileInputStream("screen-blocker.cfg");
         p.load(inputstream);
 
         return p;
@@ -159,9 +195,11 @@ class Screen implements KeyListener {
         if(e.getKeyCode() == KeyEvent.VK_ENTER){
             if (Arrays.equals(secret.toCharArray(), field.getPassword())) {
                 device.setFullScreenWindow(null);
+                logger.info("Correct secret key");
             } else {
                 field.setText("");
                 label.setText("Wrong Secret Key");
+                logger.info("Incorrect secret key");
             }
         }
     }
