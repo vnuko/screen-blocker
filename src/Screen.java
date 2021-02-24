f60cde03a7913e3850e9f5e0098d13688771a256
Milos Vnucko <milosz.milosz@gmail.com>
2021-02-24 22:45:47 +0100

    code cleanup + partially added unlock after func

--- src/Screen.java
+++ src/Screen.java
@@ -1,5 +1,8 @@
-import java.awt.event.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
 import java.util.*;
 import java.util.Timer;
@@ -7,137 +10,158 @@ import java.util.logging.FileHandler;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-import javax.imageio.ImageIO;
-import javax.swing.*;
 
-class Screen implements KeyListener {
-    // JTextField
-    static JPasswordField field;
+public class Screen implements KeyListener {
+    protected Properties config;
+    protected Logger logger;
+    protected GraphicsEnvironment graphics;
+    protected GraphicsDevice device;
+    protected JDialog frame;
+    protected JLabel background;
+    protected JPasswordField field;
+    protected JLabel label;
+    protected JLabel message;
+    protected String timeStr;
+    protected long lockTimestamp;
 
-    // JFrame
-    static JDialog frame;
+    public static void main(String[] args) {
+        Screen screenBlocker = new Screen(
+                Screen.loadConfig(),
+                Screen.getLoger(
+                        Screen.class.getName()
+                )
+        );
+        screenBlocker.init();
+    }
 
-    static JLabel label;
-    static JLabel message;
+    public Screen(Properties config, Logger logger) {
+        this.config = config;
+        this.logger = logger;
 
-    static JLabel background;
-
-    static GraphicsEnvironment graphics;
-    static GraphicsDevice device;
-
-    static Properties properties;
-
-    static String timeStr;
-
-    static Logger logger;
-    static FileHandler fh;
-
-    // main class
-    public static void main(String[] args) throws IOException {
-        System.setProperty("apple.awt.UIElement", "true");
-
-        logger = Logger.getLogger(Screen.class.getName());
-        fh = null;
-
-        try {
-            fh = new FileHandler("screen-blocker.log");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        fh.setFormatter(new SimpleFormatter());
-        logger.addHandler(fh);
-
-        properties = Screen.getProperties();
-
-        if (!properties.getProperty("debug", "false").equals("true")) {
+        if (!this.config.getProperty("debug", "false").equals("true")) {
             LogManager.getLogManager().reset(); // disable logging
         }
+    }
+
+    public void init() {
+        System.setProperty("apple.awt.UIElement", "true");
 
         try {
             UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
+                    UIManager.getSystemLookAndFeelClassName()
+            );
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        Screen scr = new Screen();
 
         graphics = GraphicsEnvironment.getLocalGraphicsEnvironment();
         device = graphics.getDefaultScreenDevice();
 
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
 
-        // Main Window Component
+        // Main Frame
         frame = new JDialog();
         frame.setUndecorated(true);
         frame.setResizable(false);
-        frame.setBackground(new Color(0,0,0));
-
-        // Background Component
-        background = new JLabel();
-        background.setIcon(
-                new ImageIcon(
-                        new ImageIcon(
-                                ImageIO.read(
-                                        new File("screen-blocker.jpg")
-                                )
-                        ).getImage().getScaledInstance((int) size.getWidth(), (int) size.getHeight(), Image.SCALE_DEFAULT)
-                )
+        frame.setBackground(
+                new Color(0,0,0)
         );
+
+        // Background
+        background = new JLabel();
+        try {
+            background.setIcon(
+                    new ImageIcon(
+                            new ImageIcon(
+                                    ImageIO.read(
+                                            new File("screen-blocker.jpg")
+                                    )
+                            ).getImage().getScaledInstance(
+                                    (int) size.getWidth(),
+                                    (int) size.getHeight(),
+                                    Image.SCALE_DEFAULT
+                            )
+                    )
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         background.setLayout(null);
 
         frame.setContentPane(background);
 
-        // Field Component
+        // Password Field
         field = new JPasswordField(8);
-        field.setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5));
-        field.setBackground(new Color(255,255, 255, 180));
-        field.setBounds((int) ((size.getWidth() / 2) - (200 / 2)), (int) ((size.getHeight() / 2) - (26 / 2)), 200, 36);
+        field.setBorder(
+                javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5)
+        );
+        field.setBackground(
+                new Color(255,255, 255, 180)
+        );
+        field.setBounds(
+                (int) ((size.getWidth() / 2) - (200 / 2)),
+                (int) ((size.getHeight() / 2) - (26 / 2)),
+                200,
+                36
+        );
         field.setFont(
                 new Font("SansSerif", Font.BOLD, 20)
         );
-        field.addKeyListener(scr);
+        field.addKeyListener(this);
 
         background.add(field);
 
-        // Field Label Component a.k.a Error Message
+        // Error Message
         label = new JLabel("");
-        label.setBounds(field.getX(), (field.getY() + field.getHeight() + 10), 200, 20);
+        label.setBounds(
+                field.getX(),
+                (field.getY() + field.getHeight() + 10),
+                200,
+                20
+        );
         label.setForeground(new Color(175, 0, 29));
 
         background.add(label);
 
-        // Message Component
-        message = new JLabel(properties.getProperty("message", ""));
+        // Custom Message
+        message = new JLabel(
+                config.getProperty("message", "")
+        );
         message.setFont(
                 new Font("SansSerif", Font.PLAIN, 100)
         );
-        message.setForeground(new Color(255, 255, 255));
+        message.setForeground(
+                new Color(255, 255, 255)
+        );
         message.setBounds(
-            (int) ((size.getWidth() / 2) - (message.getPreferredSize().getWidth() / 2)),
-            (int) ((size.getHeight() / 2) - message.getPreferredSize().getHeight() - 60),
-            (int) message.getPreferredSize().getWidth(),
-            (int) message.getPreferredSize().getHeight()
+                (int) ((size.getWidth() / 2) - (message.getPreferredSize().getWidth() / 2)),
+                (int) ((size.getHeight() / 2) - message.getPreferredSize().getHeight() - 60),
+                (int) message.getPreferredSize().getWidth(),
+                (int) message.getPreferredSize().getHeight()
         );
 
         background.add(message);
 
-        logger.info("Starting timer...");
+        this.startTimer();
 
-        Timer timer = new Timer();
+        //this.lock(); // DEBUG
+    }
+
+    protected void startTimer() {
+        java.util.Timer timer = new Timer();
+
         TimerTask tt = new TimerTask() {
             public void run() {
-                Calendar cal = Calendar.getInstance(); //this is the method you should use, not the Date(), because it is desperated.
+                Calendar cal = Calendar.getInstance();
 
                 switch (cal.get(Calendar.DAY_OF_WEEK)) {
-                    case Calendar.SUNDAY -> timeStr = properties.getProperty("sun", "");
-                    case Calendar.MONDAY -> timeStr = properties.getProperty("mon", "");
-                    case Calendar.TUESDAY -> timeStr = properties.getProperty("tue", "");
-                    case Calendar.WEDNESDAY -> timeStr = properties.getProperty("wed", "");
-                    case Calendar.THURSDAY -> timeStr = properties.getProperty("thu", "");
-                    case Calendar.FRIDAY -> timeStr = properties.getProperty("fri", "");
-                    case Calendar.SATURDAY -> timeStr = properties.getProperty("sat", "");
+                    case Calendar.SUNDAY -> timeStr = config.getProperty("sun", "");
+                    case Calendar.MONDAY -> timeStr = config.getProperty("mon", "");
+                    case Calendar.TUESDAY -> timeStr = config.getProperty("tue", "");
+                    case Calendar.WEDNESDAY -> timeStr = config.getProperty("wed", "");
+                    case Calendar.THURSDAY -> timeStr = config.getProperty("thu", "");
+                    case Calendar.FRIDAY -> timeStr = config.getProperty("fri", "");
+                    case Calendar.SATURDAY -> timeStr = config.getProperty("sat", "");
                     default -> timeStr = ""; // should never happen
                 }
 
@@ -146,8 +170,7 @@ class Screen implements KeyListener {
 
                 logger.info("Current time: " + hour  + ":" + min);
 
-                if(!timeStr.equals("")) {
-
+                if (!timeStr.equals("")) {
                     logger.info("Time configurations for today: " + timeStr);
 
                     StringTokenizer tokens = new StringTokenizer(timeStr, ";");
@@ -158,29 +181,49 @@ class Screen implements KeyListener {
                         if (hour == Integer.parseInt(hhmmArr[0]) && min == Integer.parseInt(hhmmArr[1])) {
                             logger.info("FOUND match, current time " + hour  + ":" + min + " matches config's: " + Integer.parseInt(hhmmArr[0]) + ":" + Integer.parseInt(hhmmArr[1]));
                             logger.info("Showing Screen Blocker window");
-                            device.setFullScreenWindow(frame);
+                            Screen.this.lock();
                             break;
                         } else {
                             logger.info("NO match, will try again in 60 sec. Current time: " + hour  + ":" + min + " didn't match config's: " + Integer.parseInt(hhmmArr[0]) + ":" + Integer.parseInt(hhmmArr[1]));
                         }
                     }
                 }
+
+//                if (lockTimestamp != 0 && lockTimestamp > (lockTimestamp - (2 * 60 * 1000))) {
+//                    Screen.this.unlock();
+//                }
             }
         };
         timer.schedule(tt, 1000, 1000*60);//	delay the task 1 second, and then run task every 60 seconds
-
-        device.setFullScreenWindow(frame); //DEBUG
     }
-    
-    public static Properties getProperties() throws IOException {
-        Properties p = new Properties();
 
-        InputStream inputstream = new FileInputStream("screen-blocker.cfg");
-        p.load(inputstream);
+    public static Properties loadConfig() {
+        Properties config = new Properties();
 
-        return p;
+        try {
+            InputStream inputStream = new FileInputStream("screen-blocker.cfg");
+            config.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return config;
     }
-    
+
+    public static Logger getLoger(String className) {
+        Logger logger = Logger.getLogger(className);
+
+        try {
+            FileHandler fh = new FileHandler("screen-blocker.log");
+            fh.setFormatter(new SimpleFormatter());
+            logger.addHandler(fh);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return logger;
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {
 
@@ -188,13 +231,13 @@ class Screen implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        String secret = properties.getProperty("secret");
-
+        String secret = config.getProperty("secret", "");
         label.setText("");
 
         if(e.getKeyCode() == KeyEvent.VK_ENTER){
             if (Arrays.equals(secret.toCharArray(), field.getPassword())) {
-                device.setFullScreenWindow(null);
+                field.setText("");
+                this.unlock();
                 logger.info("Correct secret key");
             } else {
                 field.setText("");
@@ -207,5 +250,15 @@ class Screen implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    protected void lock() {
+        device.setFullScreenWindow(frame);
+        lockTimestamp = System.currentTimeMillis();
+    }
+
+    protected void unlock() {
+        device.setFullScreenWindow(null);
+        lockTimestamp = 0;
     }
 }
